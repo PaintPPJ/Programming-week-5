936efd5137dc7239ddded69bb2efc5e7bfcce284
Pakanon Jaroenras <69156418+PaintPPJ@users.noreply.github.com>
2020-09-10 10:49:37 +0700

    Add files via upload

---
+++ Programming week 5.2/Programming week 5.2/Source 2.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i, j, n;
+	scanf("%d",&n);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 65; j < 64 + (2 * n); j++)
+		{
+			if (j >= (64 + n) + i) 
+			{
+				printf("%c ", (((((64 + n) - (j % (64 + n))))-65)%26)+65);
+			}
+			else if (j <= (64 + n) - i)
+			{
+				printf("%c ", ((j-65)%26)+65);
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
